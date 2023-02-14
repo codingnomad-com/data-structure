1bff17dc253fe981cef3ead14158759f287b3ef4
Manuel Bachl <manuel@coding-nomad.com>
2023-02-14 23:41:15 +0800

    adjusted schema

--- exampleSchema.ts
+++ exampleSchema.ts
@@ -13,54 +13,53 @@ export default {
             id: 'randomId3',
             headline: 'Headlines',
             slug: 'headlines',
-            rows: [
+            columns: [
+              // results in 1 column since it has only 1 object in columns
               {
                 id: 'randomId4',
-                columns: [
-                  // results in 1 column since it has only 1 object in columns
+                colWidth: {
+                  desktop: 12,
+                  tablet: 12,
+                  phone: 12,
+                },
+                components: [
                   {
                     id: 'randomId5',
-                    colWidth: 12,
-                    components: [
-                      {
-                        id: 'randomId6',
-                        component: 'Headline',
-                        props: {
-                          children: 'Headline h1',
-                          fontWeight: 'bold',
-                          headlineLevel: 'h1',
-                          textAlign: 'left',
-                          textColor: 'primary',
-                          textTransform: 'uppercase',
-                        },
-                      },
-                      {
-                        id: 'randomId7',
-                        component: 'Headline',
-                        props: {
-                          children: 'Headline h2',
-                          fontSize: '25px',
-                          fontWeight: 'normal',
-                          headlineLevel: 'h2',
-                          textAlign: 'left',
-                          textColor: 'secondary',
-                          textTransform: 'uppercase',
-                        },
-                      },
-                      {
-                        id: 'randomId7',
-                        component: 'Headline',
-                        props: {
-                          children: 'Headline h2',
-                          fontSize: '20px',
-                          fontWeight: 'normal',
-                          headlineLevel: 'h3',
-                          textAlign: 'center',
-                          textColor: 'secondaryInactive',
-                          textTransform: 'uppercase',
-                        },
-                      },
-                    ],
+                    component: 'Headline',
+                    props: {
+                      children: 'Headline h1',
+                      fontWeight: 'bold',
+                      headlineLevel: 'h1',
+                      textAlign: 'left',
+                      textColor: 'primary',
+                      textTransform: 'uppercase',
+                    },
+                  },
+                  {
+                    id: 'randomId6',
+                    component: 'Headline',
+                    props: {
+                      children: 'Headline h2',
+                      fontSize: '25px',
+                      fontWeight: 'normal',
+                      headlineLevel: 'h2',
+                      textAlign: 'left',
+                      textColor: 'secondary',
+                      textTransform: 'uppercase',
+                    },
+                  },
+                  {
+                    id: 'randomId7',
+                    component: 'Headline',
+                    props: {
+                      children: 'Headline h2',
+                      fontSize: '20px',
+                      fontWeight: 'normal',
+                      headlineLevel: 'h3',
+                      textAlign: 'center',
+                      textColor: 'secondaryInactive',
+                      textTransform: 'uppercase',
+                    },
                   },
                 ],
               },
@@ -70,40 +69,43 @@ export default {
             id: 'randomId8',
             headline: 'Paragraphs',
             slug: 'paragraphs',
-            rows: [
+            columns: [
+              // results in 2 column since it has 2 objects in columns
               {
                 id: 'randomId9',
-                columns: [
-                  // results in 2 column since it has 2 objects in columns
+                colWidth: {
+                  desktop: 4,
+                  tablet: 6,
+                  phone: 12,
+                },
+                components: [
                   {
                     id: 'randomId10',
-                    colWidth: 4,
-                    components: [
-                      {
-                        id: 'randomId11',
-                        component: 'Paragraph',
-                        props: {
-                          fontSize: '20px',
-                          lineHeight: 1.5,
-                        },
-                        children: 'Lorem ipsum dolor sit amet.',
-                      },
-                    ],
+                    component: 'Paragraph',
+                    props: {
+                      fontSize: '20px',
+                      lineHeight: 1.5,
+                    },
+                    children: 'Lorem ipsum dolor sit amet.',
                   },
+                ],
+              },
+              {
+                id: 'randomId11',
+                colWidth: {
+                  desktop: 8,
+                  tablet: 6,
+                  phone: 12,
+                },
+                components: [
                   {
-                    id: 'randomId12',
-                    colWidth: 6,
-                    components: [
-                      {
-                        id: 'randomId13',
-                        component: 'Paragraph',
-                        props: {
-                          fontSize: '22px',
-                          lineHeight: 1.6,
-                        },
-                        children: 'Lorem ipsum dolor sit amet.',
-                      },
-                    ],
+                    id: 'randomId13',
+                    component: 'Paragraph',
+                    props: {
+                      fontSize: '22px',
+                      lineHeight: 1.6,
+                    },
+                    children: 'Lorem ipsum dolor sit amet.',
                   },
                 ],
               },
